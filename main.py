db4ad0383ded3328834af042e7cc834248af0c5e
Rachel Sweeney <rsweeney@pewresearch.org>
2019-10-03 11:43:12 -0400

    I'm on a plane and can't lookup the argparse library documentation, so this will have to do for passing functionality

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from face_reco import start_video
+import sys
 
 # Add arg parser to create mode for startup
 
 ip = "rtsp://192.168.43.1:8080/h264_ulaw.sdp"
 
-start_video(ip)
+try:
+    start_video(ip, sys.argv[1])
+except:
+    start_video(ip, sys.argv[0])
